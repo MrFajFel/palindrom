21e338a954be17bbbf27de8822a57525d2934e61
MrFajFel <127607303+MrFajFel@users.noreply.github.com>
2024-02-28 20:27:39 +0100

    dsda

--- program.py
+++ program.py
@@ -3,10 +3,13 @@ x=0;
 tesktodtylu = ""
 
 # palindrom
-for i in range(len(user)):
-    x = x+1
-    odtylu = user[0-x]
-    tesktodtylu += odtylu
+if user != "":
+    for i in range(len(user)):
+        x = x+1
+        odtylu = user[0-x]
+        tesktodtylu += odtylu
 
-if tesktodtylu == user:
-    print("dziala")
+    if tesktodtylu == user:
+        print("dziala")
+else:
+    print("nie przyjmujemy pustych tekstow")
